dfc4c3fb820baaa2d151b5887247844ad83f6d4e
agzsoftsi <agzsoftsi@gmail.com>
2019-12-09 20:27:31 +0000

    Advance task 13

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,4 +1,4 @@
-#include "list.h"
+#include "lists.h"
 /**
  *check_cycle - check if exist a cycle
  *@list: list to check
@@ -6,8 +6,15 @@
  */
 int check_cycle(listint_t *list)
 {
+	listint_t *nod1 = list;
+	listint_t *nod2 = list;
 
-
-
-
+	while (nod1 && nod2 && nod2->next)
+	{
+		nod1 = nod1->next;
+		nod2 = nod2->next->next;
+		if (nod1 == nod2)
+			return (1);
+	}
+	return (0);
 }
