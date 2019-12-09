46d1a9c4d65a3f55a24c3f6380fcd3d85a35a20f
agzsoftsi <agzsoftsi@gmail.com>
2019-12-09 15:51:47 +0000

    mandatories tasks

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,13 @@
+#include "list.h"
+/**
+ *check_cycle - check if exist a cycle
+ *@list: list to check
+ *Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+
+
+
+
+}
