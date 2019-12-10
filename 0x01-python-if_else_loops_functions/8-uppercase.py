f0fce136f30915f4ea461705918a7807c10caff8
agzsoftsi <agzsoftsi@gmail.com>
2019-12-10 19:47:13 +0000

    task8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    case = 0
+    for letra in str:
+        if ord(letra) >= ord('a') and ord(letra) <= ord('z'):
+            case = 32
+        else:
+            case = 0
+        print('{:c}'.format(ord(letra) - case), end=' ')
+    print()
