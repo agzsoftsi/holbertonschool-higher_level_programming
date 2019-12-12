2186ec8266c678c9f8529c3aba770df368c195a5
agzsoftsi <agzsoftsi@gmail.com>
2019-12-12 20:18:05 +0000

    fix 2 v2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 from sys import argv
-if len(argv) == 1:
-    print('0 arguments.')
-    exit()
-numarg = len(argv) - 1
-if len(argv) == 2:
-    print('{} argument:'.format(numarg))
-else:
-    print('{} arguments:'.format(numarg))
+if __name__ == '__main__':
+    if len(argv) == 1:
+        print('0 arguments.')
+        exit()
+    numarg = len(argv) - 1
+    if len(argv) == 2:
+        print('{} argument:'.format(numarg))
+    else:
+        print('{} arguments:'.format(numarg))
 
-con = 0
-for n in argv:
-    con = con + 1
-    if con == 1:
-        continue
-    print('{}: {}'.format(con - 1, n))
+    con = 0
+    for n in argv:
+        con = con + 1
+        if con == 1:
+            continue
+        print('{}: {}'.format(con - 1, n))
