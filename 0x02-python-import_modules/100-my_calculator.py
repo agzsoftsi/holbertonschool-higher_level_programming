6770e184e2f0c8021d4d09258079d2e432429e08
agzsoftsi <agzsoftsi@gmail.com>
2019-12-13 01:07:59 +0000

    task advance 100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -18,4 +18,4 @@ if __name__ == '__main__':
     elif argv[2] == '*':
         print('{} * {} = {}'.format(a, b, mul(a, b)))
     elif argv[2] == '/':
-        print('{} - {} = {}'.format(a, b, div(a, b)))
+        print('{} / {} = {}'.format(a, b, div(a, b)))
