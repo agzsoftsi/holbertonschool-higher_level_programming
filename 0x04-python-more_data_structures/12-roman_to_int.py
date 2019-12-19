f74143ffdb6262d7a76bd7c02f7325a102cbfc16
agzsoftsi <agzsoftsi@gmail.com>
2019-12-19 23:16:55 +0000

    fix task 12 - pep8

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,7 +4,7 @@ def roman_to_int(roman_string):
         return 0
     val, total = 0, 0
     nums = []
-    legend = {'I': 1, 'V': 5, 'X': 10, 'L': 50,'C': 100,
+    legend = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,
               'D': 500, 'M': 1000}
     for n, num in enumerate(roman_string):
         nums.append(legend.get(num))
