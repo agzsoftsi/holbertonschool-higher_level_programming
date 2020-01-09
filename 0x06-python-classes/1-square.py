895762ca58a4d8ff0c3741b1a576091e2c139c40
agzsoftsi <agzsoftsi@gmail.com>
2020-01-09 20:48:14 +0000

    Task 1 and 2 with pep8

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -2,13 +2,5 @@
 class Square:
     """Class Square is created"""
 
-    def __init__(self, size=0):
+    def __init__(self, size):
         """Initializes with a size"""
-
-        if type(size) != int:
-            raise TypeError('size must be an integer')
-            """Verify that size is not an integer"""
-        if size < 0:
-            raise ValueError('size must be >= 0')
-            """Verify that size is not negative or 0"""
-        self.__size = size
