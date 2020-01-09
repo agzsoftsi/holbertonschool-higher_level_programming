f76c4b9742e15b6756cbd44c726446510f0ac0db
agzsoftsi <agzsoftsi@gmail.com>
2020-01-09 23:20:27 +0000

    Task 6

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+class Square:
+    """Class Square is created"""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Initializes with a size"""
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """method to return size value"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """method to set a size value"""
+        if type(value) != int:
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+            """Verify that size is not negative or 0"""
+        self.__size = value
+
+    def area(self):
+        """Return the area of the square"""
+        return self.__size**2
+
+    def my_print(self):
+        """Prints square with # in stdout."""
+        if self.__size == 0:
+            print()
+        else:
+            for num in range(0, self.__size):
+                print('#' * self.__size)
+                """loop to print the square"""
+    @property
+    def position(self):
+        """Get position."""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Set position Value."""
+        if type(value) != tuple or len(value) != 2 or type(value[0]) != int\
+           or type(value[1]) != int or value[0] < 0 or value[1] < 0:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        self.__position = value
