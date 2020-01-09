eebfdbab18bbd4c259cfc7ea2563af734587843d
agzsoftsi <agzsoftsi@gmail.com>
2020-01-09 20:58:28 +0000

    Task 3 with pep8

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+class Square:
+    """Class Square is created"""
+
+    def __init__(self, size=0):
+        """Initializes with a size"""
+
+        if type(size) != int:
+            raise TypeError('size must be an integer')
+            """Verify that size is not an integer"""
+        if size < 0:
+            raise ValueError('size must be >= 0')
+            """Verify that size is not negative or 0"""
+        self.__size = size
+
+    def area(self):
+        """Return the area of the square"""
+        return self.__size**2
