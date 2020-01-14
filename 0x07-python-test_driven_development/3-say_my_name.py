3e7a7312ff9a1b55bab1f71f1d2fa0c32771c809
agzsoftsi <agzsoftsi@gmail.com>
2020-01-14 01:29:23 +0000

    Cases to task 2

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This is the "say my name" module.
+This module supplies one function, say_my_name().
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    Print 'My name is <first name> <last name>'
+    """
+
+    if type(first_name) is not str:
+        raise TypeError('first_name must be a string')
+    if type(last_name) is not str:
+        raise TypeError('last_name must be a string')
+
+    print('My name is {} {}'.format(first_name, last_name))
