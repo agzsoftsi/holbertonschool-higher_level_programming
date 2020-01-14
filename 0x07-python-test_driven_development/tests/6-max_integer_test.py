ad8d89dcba479e11a04f15e85fb6422c47a1ad84
agzsoftsi <agzsoftsi@gmail.com>
2020-01-14 05:47:08 +0000

    fix cases with mistake

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -7,7 +7,7 @@ max_integer = __import__('6-max_integer').max_integer
 class TestMaxInteger(unittest.TestCase):
     """Test cases for max_integer function."""
 
-    def integers_test(self):
+    def test_int(self):
         """Test valid integers."""
         self.assertEqual(max_integer([10, -20, 30, -40]), 30)
         self.assertEqual(max_integer([50]), 50)
@@ -16,7 +16,7 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(max_integer([1, 3, 4, 2]), 4)
         self.assertEqual(max_integer([-2, -6, -8, -4]), -2)
 
-    def type_test(self):
+    def test_type(self):
         """Test other compatible data types."""
         self.assertEqual(max_integer([1, 2, 3, float('inf')]), float('inf'))
         self.assertEqual(max_integer([1, 2, 3, float('nan')]), 3)
