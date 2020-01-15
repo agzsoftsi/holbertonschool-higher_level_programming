5e5bf7b0138156fbdd138a35336fc1a528fd7a4e
agzsoftsi <agzsoftsi@gmail.com>
2020-01-15 05:47:14 +0000

    fix3 9

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -9,8 +9,8 @@ class Rectangle():
 
     def __init__(self, width=0, height=0):
         """Initialize the width and height of rectangle."""
-        self.height = height
         self.width = width
+        self.height = height
         Rectangle.number_of_instances += 1
 
     def __del__(self):
