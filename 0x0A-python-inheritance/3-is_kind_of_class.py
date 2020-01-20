3b43b55afffd054ebc67b68405ca55db88e2388c
agzsoftsi <agzsoftsi@gmail.com>
2020-01-20 20:58:44 +0000

    Task 3 done

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module to Task3."""
+
+
+def is_kind_of_class(obj, a_class):
+    """Returns True or false, if the object is an instance a class."""
+    return isinstance(obj, a_class)
