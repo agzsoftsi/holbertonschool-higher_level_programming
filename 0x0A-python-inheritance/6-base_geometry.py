f567888b708649cd6b8e9062873fbc739e4d789c
agzsoftsi <agzsoftsi@gmail.com>
2020-01-20 21:14:45 +0000

    Task 6 done

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module to task6"""
+
+
+class BaseGeometry():
+    """A class for a task6."""
+    def area(self):
+        """Use an Exception."""
+        raise Exception('area() is not implemented')
