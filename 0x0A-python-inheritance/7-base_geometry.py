9310c76db2a61e3bf12e3500e09cf95f1cb8ea3e
agzsoftsi <agzsoftsi@gmail.com>
2020-01-20 21:33:59 +0000

    Task 7 done

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module to task7."""
+
+
+class BaseGeometry():
+    """A class to task7"""
+    def area(self):
+        """Use an Exception."""
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        """To validate value and raises exceptions."""
+        if not type(value) is int:
+            raise TypeError('{} must be an integer'.format(name))
+        elif value <= 0:
+            raise ValueError('{} must be greater than 0'.format(name))
