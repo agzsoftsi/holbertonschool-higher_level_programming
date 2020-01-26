4010138b84c2b94bacb41573a3a7ba6ed636defd
agzsoftsi <agzsoftsi@gmail.com>
2020-01-26 04:31:10 +0000

    Task 8 done

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -30,6 +30,23 @@ class Rectangle(Base):
                                                 self.x, self.y, self.width,
                                                 self.height)
 
+    def update(self, *args, **kwargs):
+        """Update the rectangle: id, width, height, x, y - Task 8."""
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.__width = args[1]
+            if len(args) >= 3:
+                self.__height = args[2]
+            if len(args) >= 4:
+                self.__x = args[3]
+            if len(args) >= 5:
+                self.__y = args[4]
+        elif kwargs:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
     @property
     def width(self):
         """Getter for width. - Task 3"""
