cc8abe65f3907f3ba2f340cf2c7df93da3dbf62a
agzsoftsi <agzsoftsi@gmail.com>
2020-01-27 01:34:35 +0000

    task 16 done

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -21,3 +21,13 @@ class Base:
         if not list_dictionaries or list_dictionaries is None:
             return '[]'
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Write the JSON string representation of list_objs to a file."""
+        filename = cls.__name__ + '.json'
+        dictionary = []
+        if list_objs is not None:
+            dictionary = [obj.to_dictionary() for obj in list_objs]
+        with open(filename, 'w') as f:
+            f.write(cls.to_json_string(dictionary))
