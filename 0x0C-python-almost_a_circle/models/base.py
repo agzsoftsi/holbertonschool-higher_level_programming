697c17646ea6165c026faf714bae2cfc7e045961
agzsoftsi <agzsoftsi@gmail.com>
2020-01-27 01:01:42 +0000

    fix dictionaries for task 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Define a Base Module."""
+import json
 
 
 class Base:
@@ -13,3 +14,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Return the JSON string of list_dictionaries - Task 15"""
+        if not list_dictionaries or list_dictionaries is None:
+            return '[]'
+        return json.dumps(list_dictionaries)
