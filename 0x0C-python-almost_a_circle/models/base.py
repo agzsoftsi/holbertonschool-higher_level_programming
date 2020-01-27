d20cf9e636063a3e5a8230b97a4d5beb399e7c35
agzsoftsi <agzsoftsi@gmail.com>
2020-01-27 20:46:24 +0000

    Task 17 - 18 -19 fixed

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Define a Base Module."""
 import json
+from os import path
 
 
 class Base:
@@ -24,10 +25,37 @@ class Base:
 
     @classmethod
     def save_to_file(cls, list_objs):
-        """Write the JSON string representation of list_objs to a file."""
+        """Write the JSON string of list_objs to a file - Task 16"""
         filename = cls.__name__ + '.json'
         dictionary = []
         if list_objs is not None:
             dictionary = [obj.to_dictionary() for obj in list_objs]
         with open(filename, 'w') as f:
             f.write(cls.to_json_string(dictionary))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Return the list of the JSON string - Task 17."""
+        if not json_string or json_string is None:
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Return an instance with all attributes already set - Task 18."""
+        if cls.__name__ == 'Rectangle':
+            new_base = cls(1, 1)
+        else:
+            new_base = cls(1)
+        cls.update(new_base, **dictionary)
+        return new_base
+
+    @classmethod
+    def load_from_file(cls):
+        """Return a list of instances - Task 19."""
+        filename = cls.__name__ + '.json'
+        if path.isfile(filename):
+            with open(filename, 'r') as f:
+                my_list = cls.from_json_string(f.read())
+            return [cls.create(**obj) for obj in my_list]
+        return []
