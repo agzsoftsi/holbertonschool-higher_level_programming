633454f68ea4a1e87743df3629b6a2286427d7f2
agzsoftsi <agzsoftsi@gmail.com>
2020-01-28 02:55:53 +0000

    Task 21 Advanced

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """Define a Base Module."""
 import json
 from os import path
+import csv
 
 
 class Base:
@@ -59,3 +60,36 @@ class Base:
                 my_list = cls.from_json_string(f.read())
             return [cls.create(**obj) for obj in my_list]
         return []
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """Write the CSV of list_objs to a file - Task 21."""
+        filename = cls.__name__ + '.csv'
+        if list_objs is not None:
+            if cls.__name__ == 'Rectangle':
+                val = [[obj.id, obj.width, obj.height, obj.x, obj.y]
+                       for obj in list_objs]
+            else:
+                val = [[obj.id, obj.size, obj.x, obj.y] for obj in list_objs]
+            with open(filename, 'w', newline='') as f:
+                writer = csv.writer(f)
+                writer.writerows(val)
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """Return a list of instances. - Task 21"""
+        filename = cls.__name__ + '.csv'
+        dictionary = []
+        if path.isfile(filename):
+            with open(filename, 'r') as f:
+                reader = csv.reader(f)
+                for row in reader:
+                    if cls.__name__ == 'Rectangle':
+                        val = {'id': int(row[0]), 'width': int(row[1]),
+                               'height': int(row[2]), 'x': int(row[3]),
+                               'y': int(row[4])}
+                    else:
+                        val = {'id': int(row[0]), 'size': int(row[1]),
+                               'x': int(row[2]), 'y': int(row[3])}
+                    dictionary.append(cls.create(**val))
+        return dictionary
