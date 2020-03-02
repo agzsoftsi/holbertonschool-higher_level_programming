aaea3e0f33d6bacde8cdf845e1866ba76471a93e
agzsoftsi <agzsoftsi@gmail.com>
2020-03-02 07:44:05 +0000

    Task 12 Done

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,3 @@
+-- Updates the score of Bob to 10 in the table second_table.
+-- You are not allowed to use Bob’s id value, only the name field
+UPDATE `second_table` SET `score` = 10 WHERE `name` = "Bob";
