744d4dec1807c51bb4f69a33e60e739c402f32e3
agzsoftsi <agzsoftsi@gmail.com>
2020-03-02 18:06:00 +0000

    Task 103 Done

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,3 +1,3 @@
 -- Displays the max temperature of each state (ordered by State name)
--- Import in hbtn_0c_0 database this table dump
+-- Import in hbtn_0c_0 database this table dump - same of task 101
 SELECT state, MAX(value) AS max_temp FROM temperatures GROUP BY state ORDER BY state;
