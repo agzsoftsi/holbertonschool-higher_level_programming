665be988b39f16ad98b819cff2638c4bb7d843fc
agzsoftsi <agzsoftsi@gmail.com>
2020-03-29 17:45:14 +0000

    Task 14 - model city

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Task 14
+Write a Python file similar to model_state.py named model_city.py that contains
+the class definition of a City
+"""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class City(Base):
+    """
+    Define a City class
+    """
+    __tablename__ = 'cities'
+    id = Column(Integer, autoincrement=True, unique=True, nullable=False,
+                primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
