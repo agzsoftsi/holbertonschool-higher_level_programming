0d6c4c3a54a9beb2e9b003325e19e7615c4d8bba
agzsoftsi <agzsoftsi@gmail.com>
2020-04-15 19:44:27 +0000

    Task 10 fix pep8 change variable to shorter done

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -6,5 +6,5 @@ Task 10 - Use the Github API to display a user's ID
 if __name__ == '__main__':
     import requests
     from sys import argv
-    reqst = requests.get('https://api.github.com/user', auth=(argv[1], argv[2]))
-    print(reqst.json().get('id'))
+    req = requests.get('https://api.github.com/user', auth=(argv[1], argv[2]))
+    print(req.json().get('id'))
