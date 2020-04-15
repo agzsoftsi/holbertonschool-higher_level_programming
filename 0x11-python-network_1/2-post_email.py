e107b2470a1dc54c497e9dbe7cc54f04b140e0d1
agzsoftsi <agzsoftsi@gmail.com>
2020-04-15 17:53:48 +0000

    Task 2 -3 Fixing

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Task 2-Send a POST request to the passed URL with email as a parameter and
+display the body of the response
+"""
+
+if __name__ == '__main__':
+    import urllib.parse
+    import urllib.request
+    from sys import argv
+
+    url = argv[1]
+    values = {'email': argv[2]}
+
+    dato = urllib.parse.urlencode(values)
+    dato = dato.encode('ascii')
+    reqst = urllib.request.Request(url, dato)
+    with urllib.request.urlopen(reqst) as response:
+        body = response.read()
+    print(body.decode('utf-8'))
