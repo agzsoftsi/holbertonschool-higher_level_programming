491508c0d5dc4a07b2ff42617426673fd7311b9d
agzsoftsi <agzsoftsi@gmail.com>
2020-04-15 17:45:47 +0000

    Task 3 - fix import urllib.erro Done

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -5,14 +5,11 @@ managing HTTPError exceptions
 """
 
 if __name__ == '__main__':
+    import sys
     import urllib.request
     import urllib.error
-    from sys import argv
-
-    reqst = urllib.request.Request(argv[1])
     try:
-        with urllib.request.urlopen(reqst) as response:
-            body = response.read()
-        print(body.decode('utf-8'))
+        with urllib.request.urlopen(sys.argv[1]) as x:
+            print(x.read().decode('utf-8'))
     except urllib.error.HTTPError as e:
-        print('Error code: {}'.format(e.code))
+        print("Error code: " + str(e.code))
