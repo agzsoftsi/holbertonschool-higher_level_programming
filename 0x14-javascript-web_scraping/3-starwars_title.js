69429cd3a05398c3ea81649f1656ad8eb219f803
agzsoftsi <agzsoftsi@gmail.com>
2020-05-12 22:07:16 +0000

    Task 3 DOne

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/* Task 3 - script that prints the title of a Star Wars movie where the episode number matches a given integer. */
+/* use the module request */
+const request = require('request');
+/* use the Star wars API with the endpoint https://swapi-api.hbtn.io/api/films/:id */
+const url = 'http://swapi.co/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  if (error) throw error;
+  /*  prints the title of a Star Wars movie where the episode number matches a given integer. */
+  console.log(JSON.parse(body).title);
+});
