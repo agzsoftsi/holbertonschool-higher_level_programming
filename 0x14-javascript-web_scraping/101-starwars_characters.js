a62254e603f29df8d1aa1dff6a16e7a09a5af563
agzsoftsi <agzsoftsi@gmail.com>
2020-05-13 02:34:04 +0000

    Task 101 DOne

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
+/* script that prints all characters of a Star Wars movie */
 const request = require('request');
-
+/* first argument is the Movie ID - example: 3 = "Return of the Jedi" */
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
 const start = function () {
@@ -14,6 +15,7 @@ const end = function (characters, i) {
   if (characters.length === i) {
     return;
   }
+  /* Display one character name by line in the same order of the list "characters" in the /films/ response */
   request(characters[i], function (error, response, body) {
     if (error) throw error;
     console.log(JSON.parse(body).name);
