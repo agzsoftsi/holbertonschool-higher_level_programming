c463a858a2f3045a951bdeb1d791a2827a09f03b
agzsoftsi <agzsoftsi@gmail.com>
2020-05-14 06:34:57 +0000

    Task 1 Done

---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,5 @@
+/* Task 1 - script that updates the text color of the HTML tag HEADER to red
+   call function defined by some library Jquery in this case header
+   .css is the cascade style sheet using the property color and the color red #FF0000
+*/ 
+$('header').css('color', '#FF0000');
