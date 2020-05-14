bb0c1b17b2e58cc09db32b9bf4d65d21f2336ae8
agzsoftsi <agzsoftsi@gmail.com>
2020-05-14 07:30:04 +0000

    Task 7 Done

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,7 @@
+/* Task 7 -script that fetches and replaces the name of this URL: https://swapi-api.hbtn.io/api/people/5/?format=jsonThe name must be displayed in the HTML tag DIV#character
+You must use the jQuery API
+Use getJSON to call the API for starwars
+*/
+$.getJSON('https://swapi.co/api/people/5/?format=json', function (query) {
+  $('#character').text(query.name);
+});
