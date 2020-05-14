6a58e4d3256850ca5fac9a6e9027634ae0eceb54
agzsoftsi <agzsoftsi@gmail.com>
2020-05-14 06:21:10 +0000

    Task 0 Done

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,7 @@
+/* Task 0 - script that updates the text color of the HTML tag HEADER to red
+   selector - header
+   style for change the font color - color
+   color red - hex: #FF0000
+*/
+
+document.querySelector('header').style.color = '#FF0000';
